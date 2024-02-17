58f3169882e289cde5791fe12cf5f407a41de02a
ricky <39970880+torickyto@users.noreply.github.com>
2024-02-16 18:56:21 -0700

    Fixed error: TrackPlayer does not exist

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { AppRegistry } from 'react-native';
 import App from './App'; // Your main component
 import PlaybackService from './PlaybackService'; // Path to your PlaybackService file
 import { name as appName } from './app.json';
+import TrackPlayer from 'react-native-track-player';
 
 AppRegistry.registerComponent(appName, () => App);
 TrackPlayer.registerPlaybackService(() => PlaybackService);
